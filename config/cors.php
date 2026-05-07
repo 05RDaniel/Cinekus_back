347734b12033d9706fc10eb872b4fac8d0757d03
Dronkus 05 <daniel.rufes@gmail.com>
2026-05-07 20:21:42 +0200

    CORS

---
+++ config/cors.php
@@ -0,0 +1,14 @@
+<?php
+
+$allowedOrigins = array_filter(array_map('trim', explode(',', (string) env('CORS_ALLOWED_ORIGINS', 'http://127.0.0.1:4200,http://localhost:4200,http://127.0.0.1:5173,http://localhost:5173'))));
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => $allowedOrigins,
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
